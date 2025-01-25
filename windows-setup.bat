9ebfd2b1b699ec39da53c8ee7fb03ea9510a441f
nullptr <eliesbertran@gmail.com>
2025-01-25 17:26:39 +0100

    Added setup script :>

---
+++ windows-setup.bat
@@ -0,0 +1,18 @@
+mklink /J "%LocalAppData%/nvim" "./nvim"
+
+mkdir "%UserProfile%/.config"
+mklink /J "%UserProfile%/.config/wezterm" "./wezterm"
+mklink /J "%UserProfile%/.config/kanata" "./kanata"
+
+mkdir "%UserProfile%/.glzr"
+mklink /J "%UserProfile%/.glzr/glazewm" "./glazewm"
+mklink /J "%UserProfile%/.glzr/zebar" "./zebar"
+cd "./zebar/nullptr-bar/"
+yarn install && yarn build
+cd "../../"
+
+cd "./himawari-wallpaper/"
+pwsh setup-venv.ps1
+pwsh setup-task.ps1
+cd "../"
+
