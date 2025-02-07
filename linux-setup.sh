674204f849cd14199fed96ad2f208310d5216510
nullptr <lia@bertran.xyz>
2025-02-07 17:22:49 +0100

    updated shit

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,27 +1,26 @@
 #!/bin/env bash
 
-cd "$(dirname "$0")"
+cd "$(dirname $0)"
+dir=`pwd`
 
 function create_symlink() {
     if [ -e "$2" ]; then
         echo "Removing $2"
         rm -ri "$2"
     fi
-    ln -sf "$1" "$2"
+    ln -s "$dir/$1" "$2"
 }
 
-create_symlink ./i3 ~/.config/regolith3/i3
-create_symlink ./i3 ~/.config/i3
+create_symlink i3 ~/.config/regolith3/i3
+create_symlink i3 ~/.config/i3
 
-create_symlink ./nvim ~/.config/nvim
+create_symlink nvim ~/.config/nvim
 
-create_symlink ./kanata ~/.config/kanata
-# if [ -e /lib/systemd/system/kanata.service ]; then
-#     sudo rm /lib/systemd/system/kanata.service
-# fi
-# ln -s ./kanata/kanata.service /lib/systemd/system/
-# sudo systemctl enable kanata
+create_symlink kanata ~/.config/kanata
+create_symlink kanata/kanata.service ~/.config/systemd/user/kanata.service
+systemctl --user enable kanata
 
-create_symlink ./wezterm ~/.config/wezterm
-create_symlink ./polybar ~/.config/polybar
+create_symlink wezterm ~/.config/wezterm
+create_symlink polybar ~/.config/polybar
+create_symlink picom ~/.config/picom
 
